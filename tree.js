bff41aea2c45a1627d2670ddb1107629eea3541a
adammmusial <musialadam83@gmail.com>
2024-04-15 19:21:21 +0800

    feat: add sortedArrayToBST method for creating balanced bst

--- tree.js
+++ tree.js
@@ -1,13 +1,26 @@
 import Node from "./node";
 
 export default class Tree {
-    constructor(array){
-        this.buildTree(arr)
+    constructor(array) {
+        this.array = this.buildTree(array)
     }
 
-    buildTree(){}
+    buildTree(array, start, end) {
+        sortArr = this.sortArray(array)
+    }
 
-    sortArray(array){
+    sortedArrayToBST(arr, start, end) {
+        if (start > end) {
+          return null;
+        }
+        const mid = parseInt((start + end) / 2, 10);
+        const node = new Node(arr[mid]);
+        node.left = this.sortedArrayToBST(arr, start, mid - 1);
+        node.right = this.sortedArrayToBST(arr, mid + 1, end);
+        return node;
+      }
+
+    sortArray(array) {
         sortedArray = array.sort(sort((a, b) => a - b))
         return sortedArray
     }
