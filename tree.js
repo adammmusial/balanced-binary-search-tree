0fcf4aa7dd1905250ed8296421752a0e1fcc76c5
adammmusial <musialadam83@gmail.com>
2024-04-16 19:10:39 +0800

    feat: add levelOrder() method to perform Level-order travelsar

--- tree.js
+++ tree.js
@@ -1,8 +1,9 @@
-import Node from "./node.js";
+import {Node} from "./node.js";
 
-export default class Tree {
+class Tree {
     constructor(array) {
-        this.array = this.buildTree(array)
+        this.root = this.buildTree(array);
+        this.levelOrderTransversed = [];
     }
 
     buildTree(array) {
@@ -46,19 +47,68 @@ export default class Tree {
         return node
     }
 
+    delete(data, node = this.root) {
+        if (node == null) return node;
+    
+        if (data < node.data) node.left = this.delete(data, node.left);
+        else if (data > node.data) node.right = this.delete(data, node.right);
+        else {
+          // node with only one child or no child
+          if (node.left == null) return node.right;
+          if (node.right == null) return node.left;
+    
+          // node with two children
+          node.data = this.minValue(node.right);
+          node.right = this.delete(node.data, node.right);
+        }
+        return node;
+      }
+
     minValue(node){
         let minV = node.data
         console.log(minV)
         while (node !== null){
             minV = node.left.data
             console.log(minV)
-            node = node.left
+            node = node.left.data
             console.log(node)
         }
         return minV
     }
 
+    find(data, node = this.root){
+        if (node === undefined) return null;
+        if(node.data === data) return node
+        if (data<node.data) return this.find(data, node.left)
+        if (data>node.data) return this.find(data, node.right)
+        return "No Given Value"
+    }
+
+    levelOrder(func = this.toArray ){
+        this.levelOrderTransversed = []
+        if (this.root === null) return;
+        const queue = []
+        queue.push(this.root)
+
+        while(queue.length>0){
+            const node = queue[0];
+            func(this.levelOrderTransversed, node.data)
+            if (node.left != null) queue.push(node.left);
+            if (node.right != null) queue.push(node.right)
+            queue.shift()
+        }
+
+        return this.levelOrderTransversed
+
+    }
+
+    toArray(arr, value) {
+        arr.push(value);
+      }
 
 
-    
 }
+
+
+
+export {Tree}
